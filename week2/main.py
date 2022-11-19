609d2b8ea8b24c4a910104f4347c12cbc27d778b
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-11-19 17:13:19 +0200

    working multiple features model

--- week2/main.py
+++ week2/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+np.set_printoptions(precision=2)
+
 
 def gradient_descent(x_train, y_train, w_initial, b_initial, cost_fn, gradient_compute, number_of_iterations, alpha):
     # in cost functions list we will save all the costs obtained in the iterations
@@ -26,24 +28,23 @@ def gradient_descent(x_train, y_train, w_initial, b_initial, cost_fn, gradient_c
 
         # show progress through iterations only at 1000 iterations
         if i % math.ceil(number_of_iterations / 10) == 0:
-            print(f"Iteration: {i}: cost: {cost_functions_list[-1]} ",
-                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
-                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+            print(f"Iteration: {i}: cost: {cost_functions_list[-1]}")
 
     return w, b, cost_functions_list, w_and_b_pairs
 
 
 def compute_gradient(x_train, y_train, w, b):
     # initialize the derivatives with zero, to compute in them
-    dj_dw = 0
+    m, n = x_train.shape
     dj_db = 0
-    m = len(x_train)
+    dj_dw = np.zeros((n,))
     # iterate from 0 to m - 1 to use all the training examples
     for i in range(m):
         # use the formulas for the partial derivatives to iteratively compute
-        f_wb = np.dot(x_train[i], w) + b
-        dj_dw += (f_wb - y_train[i]) * x_train[i]
-        dj_db += (f_wb - y_train[i])
+        error = calculate_f_wb(x_train[i], w, b) - y_train[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + error * x_train[i, j]
+        dj_db += error
 
     return dj_dw / m, dj_db / m
 
@@ -82,31 +83,41 @@ def main():
     # prices are in $1000's
     y_train = np.array([100, 234, 567, 345, 123, 976, 456, 123, 264, 476, 654, 345])
     # now the x_train will have multiple features: land print (in 100s sqm), no of bedrooms,
-    # no of bathrooms, year of construction
+    # no of bathrooms, ageOfHome
 
     # each row represents the features for one training example
-    # cols will be in order: land print, noOfBedrooms, noOfBathrooms, yearOfConstruction
-    x_train = np.array([[4, 2, 1, 2000],
-                       [6, 3, 2, 2005],
-                       [10, 5, 3, 2010],
-                       [7, 4, 2, 2005],
-                       [3, 2, 1, 1995],
-                       [15, 5, 3, 2015],
-                       [8, 3, 2, 2018],
-                       [5, 3, 1, 1985],
-                       [6, 3, 2, 1980],
-                       [10, 3, 2, 1998],
-                       [15, 4, 2, 2015],
-                       [7, 4, 2, 2005],
-    ])
+    # cols will be in order: land print, noOfBedrooms, noOfBathrooms, ageOfHome
+    x_train = np.array([[4, 2, 1, 22],
+                       [6, 3, 2, 17],
+                       [10, 5, 3, 12],
+                       [7, 4, 2, 17],
+                       [3, 2, 1, 27],
+                       [15, 5, 3, 7],
+                       [8, 3, 2, 3],
+                       [5, 3, 1, 40],
+                       [6, 3, 2, 42],
+                       [10, 3, 2, 24],
+                       [15, 4, 2, 7],
+                       [7, 4, 2, 17]])
+
+    w_init = np.zeros((x_train.shape[1],))
+    b_init = 0
+    alpha = 5.0e-7
+    num_of_iterations = 200000
 
     # run the model
-    w, b, cost_functions_list, w_and_b_pairs = gradient_descent(x_train, y_train, 0, 0, cost_function, compute_gradient, 10000, 0.1)
+    w, b, cost_functions_list, w_and_b_pairs = gradient_descent(x_train,
+                                                                y_train,
+                                                                w_init,
+                                                                b_init,
+                                                                cost_function,
+                                                                compute_gradient,
+                                                                num_of_iterations,
+                                                                alpha)
 
-    print(f"costs history: {cost_functions_list}",
-          f"w and b pairs: {w_and_b_pairs}")
+    print(f"costs history: {cost_functions_list}")
 
-    sample_input_for_prediction = np.array([7, 4, 2, 2005])
+    sample_input_for_prediction = np.array([7, 4, 2, 17])
     print(f"prediction for input: {sample_input_for_prediction} is:",
           f"{calculate_f_wb(sample_input_for_prediction, w, b)}")
 
